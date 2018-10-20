2a789c6328eded49f3a66a060ef35fe1ff70d1f1
= <=>
2018-10-20 22:46:36 +0530

    fixed part_1 bug

--- part_1/views.py
+++ part_1/views.py
@@ -7,10 +7,6 @@ from .models import AppKeys
 import tweepy
 from twython import Twython
 
-# consumer_key = "at4Esg9twYr0uVm3JSZw3TTGA"
-# consumer_secret = "D4XmTaq1Z0v1WggioCmAO8bNwVnir56ZZq1moUTWwanHSzC4kT"
-# access_token = "760167659254190080-RrL0UoHrkD7QfnyWWosNasbyTYHUsK2"
-# access_token_secret = "2C8TVpS8D3vDW4BgN5NVoxJPxw2Ncf1l0mhMrsyfNKbgg"
 
 def get_followers_ids(user_id, consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -38,27 +34,23 @@ def simple_upload(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         username = fs.save('media/document/'+ myfile.name, myfile)
-        # print(username)
-        uploaded_file_url = fs.url(username)
+        uploaded_file_url = fs.url(username[5:])
+        print('hi')
+        print(uploaded_file_url)
+        print('bye')
         usernames = read_username(username)
-        #user = api.get_user(screen_name = username)
-        #print (user)
-        # namelist = []
         dictio = {}
         appKeys = AppKeys.objects.all().order_by('-id')[0]
         consumer_key = appKeys.CustomerKey
         consumer_secret = appKeys.CustomerSecretKey
         access_token = appKeys.AccessTokenKey
         access_token_secret = appKeys.AccessTokenSecret
-        #AppKeys.objects.all().delete()
         file_url = []
         list1 = []
         dictio = {}
         for username in usernames:
             if len(username) != 0:
-                print (username)
-                file_url.append(fs.url('media/document/'+username+'.txt'))
-                print(file_url)
+                file_url.append(fs.url('/document/'+username+'.txt'))
                 twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
                 output = twitter.lookup_user(screen_name=username)
                 userid =  output[0]["id_str"]
@@ -66,7 +58,6 @@ def simple_upload(request):
                 ids = get_followers_ids(userid, consumer_key, consumer_secret, access_token, access_token_secret)
                 list2=[]
                 file_to_write = 'media/document/'+username + ".txt"
-                # list10.append(file_to_write)
                 with open (file_to_write, 'w') as f:
                     for id in ids:
                         list2.append(str(id))
@@ -75,21 +66,10 @@ def simple_upload(request):
                 print ("Number of users found: " + str(len(ids)))
                 list1.append(list2)
         dictio = dict(zip(file_url, list1))
-        print(dictio)
-        #print(uploaded_file_url)
-        # return render(request, 'part_1/index.html', {
-        #     'uploaded_file_url': uploaded_file_url
-        #     })
         return render(request, 'part_1/followers.html', {
             'followers': dictio
             })
     return render(request, 'part_1/index.html')
-
-# def handle_uploaded_file(f):
-#     with open('media/name.txt', 'wb+') as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
-
 
 def getKeys(request):
     if request.method == 'POST':
@@ -97,7 +77,6 @@ def getKeys(request):
         app_keys_form = AppKeysForm(request.POST)
         if app_keys_form.is_valid():
             app_keys_form.save()
-            # return render(request, 'part_1/index.html')
             return render(request, 'part_1/buttons.html')
         else:
             app_keys_form = AppKeysForm()
